050ba3969619df96b599a9b24bd9d526c9df1ac3
philanton <jellyskate1999@gmail.com>
2021-08-22 19:23:43 +0300

    Create HTML&tailwindcss site

---
+++ README.md
@@ -0,0 +1,6 @@
+# My Birthday Site
+
+Follow these steps:
+0. Run `npm install`
+0. Run 'npm run build`
+0. Entrypoint - welcome.html
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+    },
+  },
+  plugins: [
+    //require('@tailwindcss/forms')
+  ],
+}
